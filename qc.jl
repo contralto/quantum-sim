33417e1ca871f3502cc09a209af3dc70313c083b
contralto <claudia.gonciulea@gmail.com>
2020-12-30 19:58:05 -0500

    added functions (pixel_state and complex_to_hsv) to plot state as pixels and help update graph colors with amplitudes

--- qc.jl
+++ qc.jl
@@ -6,9 +6,7 @@ qc:
 =#
 
 
-# TODO: show_graph, plot_state, pixel_state, complex_to_rgb, hsv_to_rgb
-
-using LinearAlgebra, Gadfly
+using LinearAlgebra, Gadfly, Colors
 
 const Amplitude = ComplexF64
 const State = Vector{Amplitude}
@@ -17,9 +15,10 @@ const Gate = Array{Array{Amplitude, 2}, 1}
 h = [[ComplexF64(1/sqrt(2)) 1/sqrt(2)],
      [1/sqrt(2)            -1/sqrt(2)]]
 
-function phase(theta)
+function phase(theta::ComplexF64)
     return [[1 0], [0 (cos(theta) + im * sin(theta))]]
 end
+
 
 function param_encoding(state, targets, v)
     theta = v * 2 * pi / (2 ^ length(targets))
@@ -32,7 +31,7 @@ function param_encoding(state, targets, v)
         transform!(state, i, phase(2 ^ (1im * theta)))
     end
 
-    # iqft
+#     iqft(state, targets)
 end
 
 function pair_exchange!(state::State, gate::Gate, m0::Int, m1::Int)
@@ -93,17 +92,48 @@ function transform_with_matrix!(state::State, target::Int, gate::Gate,
             end
         end
     end
-#     @show G * state
-#     show(stdout, "text/plain", G)
-#     println()
-#     println()
     state .= G * state
 end
 
-function plot_state(state)
+# https://gist.github.com/eyecatchup/9536706 Colors
+function complex_to_hsv(z::Complex, change_sat::Bool = true)
+    r = real(z)
+    i = imag(z)
+
+    val = 1.0
+
+    hue = angle(z) * 180 / pi
+    if (hue < 0) hue += 360 end
+
+    if (change_sat) sat = abs(z)
+    else sat = 1.0 end
+
+    # Make sure our arguments stay in-range
+    hue = max(0, min(360, hue))
+    sat = max(0.0, min(1.0, sat))
+    val = max(0, min(100, val))
+
+    return (hue, sat, val)
+end
+
+function bar_state(state, amp::Bool = true)
     l = length(state)
     outcomes = collect(0:l)
-    probabilities = round.(abs.(state).^2, digits = 3)
+    #amplitudes (colors based on vector)
+    if (amp)
+        data = round.(abs.(state), digits = 3)
+        bar_colors = [HSV(h, 1, v) for (h, _, v) in complex_to_hsv.(Complex.(state))]
+        ylabel = "Amplitude"
+        title = "Outcome Amplitudes"
+
+    #probabilities (all red)
+    else
+        data =round.(abs.(state).^2, digits = 3)
+        bar_colors = [HSV(0,1,1) for a in state]
+        ylabel = "Probability"
+        title = "Outcome Probabilities"
+    end
+
 
     tufte_bar = Theme(
         default_color = colorant"green",
@@ -112,20 +142,49 @@ function plot_state(state)
         grid_line_width = 0.25pt,
         minor_label_font = "Gill Sans",
         major_label_font = "Gill Sans",
-        alphas = [0.7],
+        alphas = [0.7], key_position=:none
     )
 
     set_default_plot_size(max(10, l) * cm, 10 * cm)
-
     plot(x = [string(i) * " = " * string(i, base=2, pad=Int(log2(l))) for i in 0:(l - 1)],
-         y = probabilities, label=string.(probabilities),
+         y = data, label=string.(data),
          Guide.xticks(ticks = outcomes, orientation = :vertical),
          Guide.yticks(ticks=[i * 0.1 for i = 0:10]),
          Coord.cartesian(ymin = 0.0, ymax = 1.1),
          Geom.bar, Geom.label(position=:above),
          Guide.xlabel("Outcomes"),
-         Guide.ylabel("Probability"),
-         Guide.title("Outcome Probabilities"), tufte_bar)
+         Guide.ylabel(ylabel),
+         Guide.title(title),
+         color=0:l-1,
+         Scale.color_discrete_manual(bar_colors...),
+         tufte_bar)
+end
+
+function pixel_state(state::State)
+l = size(state)[1]
+outcomes = [0:l;]
+pixel_colors = [HSV(h, s, v) for (h, s, v) in complex_to_hsv.(Complex.(state))]
+
+tufte_bar = Theme(
+default_color=colorant"gray",
+background_color=colorant"white",
+bar_spacing=1pt,
+grid_line_width=0pt,
+minor_label_font="Gill Sans",
+major_label_font="Gill Sans",
+key_position=:none
+)
+
+set_default_plot_size(4cm, max(10, l)*cm)
+probabilities = round.(abs.(state).^2, digits=3) #[abs(state[i+1])^2 for i=0:l-1]
+
+plot(y=[string(i)*"="*string(i, base=2, pad=Int(log2(l))) for i in (0:l-1)], x=[0.05 for _=0:l-1],
+Guide.ylabel(""), Guide.yticks(ticks=outcomes),
+Guide.xlabel(""), Guide.xticks(ticks=nothing),
+Coord.cartesian(xmin=0.0, xmax=0.1),
+Geom.bar(orientation=:horizontal),
+tufte_bar,
+color=0:l-1, Scale.color_discrete_manual(pixel_colors...))
 end
 
 
@@ -142,12 +201,22 @@ f(x::Int) = x % 2 == 0
 function test_play()
     n = 3
     state = init_state(n)
-    for t in 0:n - 1
+
+    for t in 0:(n)
         transform_with_matrix!(state, t, h)
     end
-    @show state
-    param_encoding(state, 1, 1)
-    plot_state(state)
+
+    # rotation of qbits with "1" in bitstring position t
+    # ex. transform!(state, 2, phase(pi/4)) rotates 100, 101, 110, 111 by pi/4
+    transform!(state, 1, phase(pi/4))
+    transform!(state, 2, phase(pi/4))
+
+#     param_encoding(state, [1], 1)
+    draw(SVG("probabilities.svg", max(10, length(state)) * cm, 10cm), bar_state(state))
+    draw(SVG("state.svg", 4cm, max(10, length(state)) * cm), pixel_state(state))
+
+#     bar_state(state)
+#     pixel_state(state)
 end
 
 test_play()
